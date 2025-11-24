6ee9271a6aeb332a647d0171ed9eeffed7ac7424
ihebtmasek-bit <ihebtmasek@gmail.com>
2025-11-24 23:20:28 +0100

    Clean CalculatorPro project - minimal structure

---
+++ app/src/main/java/com/calculatorpro/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.calculatorpro.app;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/calculator.html");
+        setContentView(webView);
+    }
+}
