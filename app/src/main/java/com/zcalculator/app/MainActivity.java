0c57090a138254aaa89c1764a0b5abbd17e02427
ihebtmasek-bit <ihebtmasek@gmail.com>
2025-11-25 00:17:14 +0100

    Complete: Add custom calculator icon, fix MainActivity location, and update package structure

---
+++ app/src/main/java/com/zcalculator/app/MainActivity.java
@@ -0,0 +1,33 @@
+package com.zcalculator.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webView = new WebView(this);
+        
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
